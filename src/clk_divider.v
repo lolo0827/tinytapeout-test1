c607a9297a7856ae690f6a3c16a58e6e8d081d73
Austin <austinlo827@gmail.com>
2023-10-15 22:41:12 -0700

    change to simplify version. And back to 2x2

--- src/clk_divider.v
+++ src/clk_divider.v
@@ -1,4 +1,4 @@
-module clk_divider (
+/*module clk_divider (
 	input i_clkPin,
 	input i_ena,
 	input wire[7:0] i_indexSelectLine,
@@ -9,7 +9,7 @@ module clk_divider (
 	reg [32:0] n;
 	
 	always @(*) begin
-		// Change n in respect to i_indexSelectLine
+    	// Change n in respect to i_indexSelectLine
       // Map the 8-bit input range to the desired output range
       // If i_indexSelectLine is 0, set n to a default value (1, which means no division)
 		 n = i_indexSelectLine == 0 ? 1 : ((20000000 - 1) * i_indexSelectLine / 255) + 1;
@@ -20,7 +20,7 @@ module clk_divider (
 		//	8'b00000010: n = 10000000;
 			// Add more cases as needed
 		//	default: n = 1000000000; // Default value of n
-		//endcase
+	    //endcase
 		
 	end
 	
@@ -38,4 +38,64 @@ module clk_divider (
 		end
 	end
 	
+endmodule*/
+
+/* Does not pass timing
+module clk_divider (
+    input i_clkPin,
+    input i_ena,
+    input wire[7:0] i_indexSelectLine,
+    output reg o_clkPin);
+
+    reg [32:0] n = 1;
+
+    reg t_internal = 0;
+    reg[32:0] counts = 0;
+
+    always @ (posedge i_clkPin) begin
+        if (i_ena) begin
+            counts = counts + 1;
+            if (counts == n) begin
+                n = i_indexSelectLine == 0 ? 1 : ((20000000 - 1) * i_indexSelectLine / 255) + 1;
+                t_internal = !t_internal;
+                counts = 0;
+            end
+            o_clkPin = t_internal;
+        end
+    end
+
+endmodule*/
+
+module clk_divider (
+    input i_clkPin,
+    input i_ena,
+    input wire[7:0] i_indexSelectLine,
+    output reg o_clkPin,
+    input i_rst_n); // active low reset signal
+
+    reg [32:0] n_next, n;
+
+    always @(*) begin
+        n_next = i_indexSelectLine == 0 ? 1 : ((20000000 - 1) * i_indexSelectLine / 255) + 1;
+    end
+
+    reg t_internal = 0;
+    reg[32:0] counts = 0;
+
+    always @ (posedge i_clkPin or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            n <= 1;
+            counts <= 0;
+            t_internal <= 0;
+        end else if (i_ena) begin
+            counts <= counts + 1;
+            if (counts == n) begin
+                n <= n_next;
+                t_internal <= !t_internal;
+                counts <= 0;
+            end
+            o_clkPin <= t_internal;
+        end
+    end
+
 endmodule
